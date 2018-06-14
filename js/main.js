4c51bdc480bcfa8a2b72e503a859fc94e28d5811
Vanals <marco91vanali@gmail.com>
2018-06-14 14:58:38 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*  Autumn Greeting Card -- js */
+
+(function($){
+	'use strict';
+
+	// declare actors here
+
+	// clear stage
+
+	// enter floor vegetation
+
+	// enter tree ..stuff
+
+	// enter the greeting text
+
+	// the GO function ...to kick things all off
+
+
+})(jQuery);
